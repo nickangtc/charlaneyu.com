db24729344538f83ce0e3eb4426dc075c8f7e4d5
Nick Ang <nickangtc@gmail.com>
2023-01-25 06:41:34 +0100

    Abstract Home into its own page file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
 import "./App.css";
-import assets from "./assets/assets.json";
 import About from "./About";
-import Image from "./components/Image";
-import Slideshow from "./components/Slideshow";
+import Home from "./pages/Home";
 
 function Header() {
   return (
@@ -81,29 +79,6 @@ function App() {
       </div>
 
       <Footer />
-    </div>
-  );
-}
-
-function Home() {
-  const MAX_IMAGES_TO_SHOW = 8;
-  const FOLDER_NAME = "home";
-
-  const images = assets[FOLDER_NAME].slice(0, MAX_IMAGES_TO_SHOW - 1).map(
-    (file, index) => {
-      return (
-        <Image path={`home/${file.name}`} key={index} className="w-full" />
-      );
-    }
-  );
-
-  return (
-    <div className="container pl-4 pr-4">
-      <Slideshow>{images}</Slideshow>
-      <p className="text-center text-3xl mb-5 italic font-serif">
-        Love of beauty is taste. The creation of beauty is art.
-      </p>
-      <p className="text-center text-lg mb-10">Ralph Waldo Emerson</p>
     </div>
   );
 }
